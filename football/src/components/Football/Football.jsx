3a9ff33faa315ce88fd2d9c9a1fcaeb16d553be2
shohagnawaz <snawazdigital@gmail.com>
2023-09-13 18:31:59 +0600

    faAwesome

--- football/src/components/Football/Football.jsx
+++ football/src/components/Football/Football.jsx
@@ -3,12 +3,17 @@ import './Football.css';
 import Player from '../Player/Player';
 
 const Football = () => {
-    const [players, setPlayers] = useState([])
+    const [players, setPlayers] = useState([]);
+    const [addPlayer, setAddPlayer] = useState([]);
     useEffect(()=>{
         fetch('player.json')
         .then(res => res.json())
         .then(data => setPlayers(data))
-    },[])
+    },[]);
+    const handleAddToPlayer = (player) => {
+        const newAddPlayer = [...addPlayer, player]
+        setAddPlayer(newAddPlayer);
+    }
 
     return (
         <div className='football-container'>
@@ -17,11 +22,13 @@ const Football = () => {
                     players.map(player => <Player
                         key = {player.id}
                         player = {player}
+                        handleAddToPlayer = {handleAddToPlayer}
                     ></Player>)
                 }
             </div>
             <div className='review-container'>
-                <h4>Review</h4>
+                <h4>Review Summary</h4>
+                <p>Selected Player: {addPlayer.length}</p>
             </div>        
         </div>
     );
--- football/src/components/Player/Player.jsx
+++ football/src/components/Player/Player.jsx
@@ -1,14 +1,26 @@
+import ReactDOM from 'react-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faFootballBall } from '@fortawesome/free-solid-svg-icons'
 import './Player.css';
 
 const Player = (props) => {
     const {name, price, img, club} = props.player;
+    const handleAddToPlayer = props.handleAddToPlayer;
 
     return (
         <div className='player'>
             <img src={img} alt="" />
-            <h4>Name: {name}</h4>
-            <p>Price: ${price}M</p>
-            <p>Club: {club}</p>
+            <div className='player-info'>
+                <h4>Name: {name}</h4>
+                <p>Price: ${price}M</p>
+                <p>Club: {club}</p>
+            </div>
+            <button 
+                onClick={() => handleAddToPlayer(props.player)} 
+                className='btn-player'>
+                Add to Player
+                <FontAwesomeIcon icon={faFootballBall} />
+            </button>
         </div>
     );
 };
